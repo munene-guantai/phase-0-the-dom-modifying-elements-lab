a72d4c1ec1d7c8c251e181e97842278bb3a41d1c
Mikebrian <mikemunene1173@gmail.com>
2023-08-02 14:10:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+    const mainNode = document.getElementById('main');
+    if (mainNode) {
+        mainNode.remove();
+    }
+
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+
+    const yourName = 'MIKE';
+    newHeader.innerHTML = '${yourName} is the champion';
+    
+    document.body.appendChild(newHeader);
